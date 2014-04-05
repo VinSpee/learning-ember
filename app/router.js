3d5aea8d68ada79674d486536d231c5f034b3fd4
Vince Speelman <v@vinspee.me>
2014-04-05 08:42:24 -0400

    implement master detail

--- app/router.js
+++ app/router.js
@@ -1,4 +1,4 @@
-var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
+var Router = Ember.Router.extend();
 
 Router.map(function() {
 	this.resource("users", function() {
